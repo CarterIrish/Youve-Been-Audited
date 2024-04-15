eaf8f5a88c7567c33ec20399cea43ab137a1f3de
JesseMaty <jqm1828@rit.edu>
2024-04-14 20:38:17 -0400

    Trap places more centered

--- YouveBeenAudited/Player.cs
+++ YouveBeenAudited/Player.cs
@@ -93,7 +93,7 @@ namespace YouveBeenAudited
             if (SingleKeyPress(Keys.Space) && _money >= 20)
             {
                 _money -= 20;
-                trap = new Trap(_position.X, Position.Y + Position.Height / 3, _nailTexture, 20, 100);
+                trap = new Trap(_position.X - 10, _position.Y + Position.Height / 6, _nailTexture, 20, 100);
             }
             else if (SingleKeyPress(Keys.D1))
             {
